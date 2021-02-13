56a4460d1cc34ecb3e95b16a69bb1c075040714e
mb16biswas <64213233+mb16biswas@users.noreply.github.com>
2021-02-13 22:19:18 +0530

    Add files via upload

---
+++ frontend/Admin.js
@@ -0,0 +1,57 @@
+import React ,{Component} from 'react' 
+import {Redirect} from "react-router-dom"
+import Mainpage from "./Mainpage"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Admin extends Component{
+    constructor(){
+        super()
+        let a = ""
+        let data = localStorage.getItem("mydata")
+        data = JSON.parse(data)
+        
+        if(data !== null){
+            a = "admin"
+        }
+
+
+
+        this.state = {
+            test : a 
+
+        }
+    }
+    logout = () =>{
+        localStorage.removeItem("mydata")
+        this.setState({test : "logout"})
+
+    }
+
+
+    render(){
+        if(this.state.test === "admin"){
+            return(
+                <div>
+                    <button onClick = {this.logout} className = "btn btn-danger" style={{display: 'flex', justifyContent: 'flex-end'}} >logout</button>                    
+                    <Mainpage/>
+                    
+
+                </div>
+              
+            )
+
+        }
+        else if(this.state.test === "logout"){
+            return(
+                <Redirect to = "/"/>
+            )
+
+        }
+        // else{
+        //     <Board/>
+        // }
+        
+    }
+} 
+
+export default Admin 
